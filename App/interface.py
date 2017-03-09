57afab8e163b03703848d3a85a4884642ae53a0a
Alejandro González <aege21@gmail.com>
2017-03-09 14:49:26 -0300

    Update interface.py

--- App/interface.py
+++ App/interface.py
@@ -4,43 +4,44 @@ import ttk
 from tkSearch import *
 from tkDelete import *
 
+#Strings used below to assign name of the tabs
 b = "Busqueda"
 t1 = "Keyword"
 t2 = "Usuario"
 t3 = "Procesos"
 processes = []
 
-#Main window
+#TwitterApp Class: This is the main window of our application and is used to manage a great part of the program
 class TwitterApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-
-        #Title inside window on top
-        self.topLabel = tk.Label(self, text = "Twitter App")
+        
+        self.topLabel = tk.Label(self, text = "Twitter App")       #Title of the window
         self.topLabel.pack()
 
-        #Tab interface
-        self.tab = ttk.Notebook(self)
+        self.tab = ttk.Notebook(self)                              #Tabs interface
         
-        #Adding titles to tabs
-        titles = [b, t3]
-        self.frames = {}
+        titles = [b, t3]                                           #Titles of the tabs
+        self.frames = {}                                           #Frames for use the tabs
         for title in titles:
             self.frames[title] = ttk.Frame(self.tab)
             self.tab.add(self.frames[title], text = title)
 
-        #Frame for search
-        self.searchKey = toSearch(self.frames[b], b, self)
+        self.searchKey = toSearch(self.frames[b], b, self)         #Search Frame
         self.searchKey.pack()
 
         #Frame for list of processes
-        self.deleteList = toDelete(self.frames[t3], self)
+        self.deleteList = toDelete(self.frames[t3], self)          #Delete List Frame
         self.deleteList.pack()
         
         self.tab.pack()
         
 
 app = TwitterApp()
+
+#Title of the program
 app.title("Twitter App")
-app.geometry('565x380')
+
+#Set window size here
+app.geometry('565x380')                               
 app.mainloop()
